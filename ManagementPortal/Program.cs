ab1da383861abf9771c12d7a0125bff594db8fd7
tanuj <tanuj@xpresso.cash>
2020-08-14 06:20:50 +0000

    fixes

--- ManagementPortal/Program.cs
+++ ManagementPortal/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -22,6 +23,24 @@ namespace ManagementPortal
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseKestrel(
+                        options =>
+                        {
+                            var config = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json", optional: false)
+                             .Build();
+                            //AWSParamStore objParamStore = new AWSParamStore();
+                            string sslCertificateName = "ManagementPortal.pfx";//objParamStore.GetValueFromParamStore(DataConstants.ParamStoreSSLCertificateName).Result;
+                            string strPortNumber = "443";//objParamStore.GetValueFromParamStore(DataConstants.ParamStoreSSLServicePort).Result;
+                            string strPassword = "Genx123!@#"; //objParamStore.GetValueFromParamStore(DataConstants.ParamStoreSSLCertificatePassword).Result;
+                            int portNumber = Convert.ToInt32(strPortNumber);
+                            options.Listen(IPAddress.Any, portNumber, listenOptions =>
+                            {
+                                listenOptions.UseHttps(sslCertificateName, strPassword);
+
+                            });
+                        }
+                                      );
                     webBuilder.UseStartup<Startup>();
                 });
     }
